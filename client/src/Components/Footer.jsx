74cab613cf796b6a676e2097e118da498e95739d
Ankush <ankushtsalian@gmail.com>
2023-01-17 22:31:12 +0530

    -------------Created footer initial---------------

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,8 +1,34 @@
 import React from "react";
-import { FooterWrapper } from "../Styles/Footer";
-
+import {
+  FooterWrapper,
+  FooterContainer,
+  FooterValue,
+  FooterValueSvg,
+  FooterNav,
+} from "../Styles/Footer";
+import Footer1 from "../Assets/Footer1.svg";
+import Footer2 from "../Assets/Footer2.svg";
+import Footer3 from "../Assets/Footer3.svg";
+import Footer4 from "../Assets/Footer4.svg";
+import Footer5 from "../Assets/Footer5.svg";
+import { footerData } from "../Utils/footerData";
 const Footer = () => {
-  return <FooterWrapper>Footer</FooterWrapper>;
+  return (
+    <FooterWrapper>
+      <FooterContainer>
+        {footerData.map((data, index) => {
+          return (
+            <FooterNav key={index}>
+              {data?.svg && (
+                <FooterValueSvg src={data?.svg} alt="./Assets/Footer1.svg" />
+              )}
+              <FooterValue>{data.value}</FooterValue>
+            </FooterNav>
+          );
+        })}
+      </FooterContainer>
+    </FooterWrapper>
+  );
 };
 
 export default Footer;
--- client/src/Styles/Footer.js
+++ client/src/Styles/Footer.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const FooterWrapper = styled.div`
+const FooterWrapper = styled.footer`
   position: relative;
   margin-top: 56px;
   border: 1px solid black;
@@ -8,6 +8,34 @@ const FooterWrapper = styled.div`
   z-index: 901;
   background-color: var(--color-grey-grade5);
   color: var(--color-white-bg);
-  min-height: 100px;
+  height: 80px;
 `;
-export { FooterWrapper };
+
+const FooterContainer = styled.div`
+  margin: 0 auto;
+  height: 100%;
+  max-width: 1248px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const FooterValue = styled.div`
+  margin-left: 5px;
+`;
+const FooterNav = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+const FooterValueSvg = styled.img`
+  margin-right: 5px;
+`;
+
+export {
+  FooterWrapper,
+  FooterNav,
+  FooterContainer,
+  FooterValue,
+  FooterValueSvg,
+};
