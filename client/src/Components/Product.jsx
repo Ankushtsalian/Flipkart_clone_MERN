9a0fc2dc2c46c0729e02b5eb2477cd743aec67f8
Ankush <ankushtsalian@gmail.com>
2023-01-19 20:55:11 +0530

    -------------------created ProductList container to hold menu--------------------------

--- client/src/Components/Product.jsx
+++ client/src/Components/Product.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-// import {chevron}
-import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import {
   ProductMenu,
@@ -8,55 +6,32 @@ import {
   ProductMenuWrapper,
   StyledProductHeaderWrapper,
 } from "../Styles/ProductHeader";
+import ProductList from "./ProductList";
 const Product = () => {
   return (
     <>
       <StyledProductHeaderWrapper>
         <ProductMenuWrapper>
-          <ProductMenuContainer>
-            <button>
-              Services
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M12 15.375L6 9.375L7.075 8.3L12 13.25L16.925 8.325L18 9.4L12 15.375Z"
-                  fill="#F9F9F9"
-                />
-              </svg>
-            </button>
-            <ProductMenu>
-              <button>UX/UI Design</button>
-              <button>Web Applications</button>
-              <button>SEO Advice</button>
-            </ProductMenu>
-          </ProductMenuContainer>
-          <ProductMenuContainer>
-            <button>
-              Services
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M12 15.375L6 9.375L7.075 8.3L12 13.25L16.925 8.325L18 9.4L12 15.375Z"
-                  fill="#F9F9F9"
-                />
-              </svg>
-            </button>
-            <ProductMenu>
-              <button>UX/UI Design</button>
-              <button>Web Applications</button>
-              <button>SEO Advice</button>
-            </ProductMenu>
-          </ProductMenuContainer>
+          {new Array(15).fill(0).map((a, i) => (
+            <ProductMenuContainer>
+              <button>
+                Services
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M12 15.375L6 9.375L7.075 8.3L12 13.25L16.925 8.325L18 9.4L12 15.375Z"
+                    fill="#F9F9F9"
+                  />
+                </svg>
+              </button>
+              <ProductList />
+            </ProductMenuContainer>
+          ))}
         </ProductMenuWrapper>
       </StyledProductHeaderWrapper>
     </>
---
+++ client/src/Components/ProductList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ProductMenu } from "../Styles/ProductHeader";
+
+const ProductList = () => {
+  return (
+    <ProductMenu>
+      <button>UX/UI Design</button>
+      <button>Web Applications</button>
+      <button>SEO Advice</button>
+    </ProductMenu>
+  );
+};
+
+export default ProductList;
