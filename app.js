9960768675d0a9c6455a32d3cece59df6958aa68
Ankush <ankushtsalian@gmail.com>
2023-01-24 12:00:28 +0530

    Server setup with db connection

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+require("dotenv").config();
 const express = require("express");
+const connectDB = require("./db/connect");
 const app = express();
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
