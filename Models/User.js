fb6a17df2f3b976bedd0eceff0b48ce253ea715e
Ankush <ankushtsalian@gmail.com>
2023-01-24 12:35:18 +0530

    user Model new objects

--- Models/User.js
+++ Models/User.js
@@ -30,6 +30,12 @@ const userSchema = new mongoose.Schema({
     enum: ["admin", "user"],
     default: "user",
   },
+  verificationToken: String,
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verified: Date,
 });
 
 userSchema.pre("save", async function () {
