ce66ac1c3c47b98c157a0dea80acf4fc4c441984
Ankush <ankushtsalian@gmail.com>
2023-01-24 14:32:56 +0530

    AUTH setup for register

--- Controllers/Auth.js
+++ Controllers/Auth.js
@@ -9,6 +9,11 @@ const register = async (req, res) => {
   if (emailAlreadyExists)
     throw new CustomError.BadRequestError("Email Already Exists");
 
+  // check if account is first in dB if so make role as ADMIN
+
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  req.body.role = isFirstAccount ? "Admin" : "User";
+
   const user = await User.create(req.body);
 
   const tokenPayload = createTokenUser(user);
