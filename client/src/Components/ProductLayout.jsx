fae8f9c0006ba901fc6c23b71461fcab09e351ba
Ankush <ankushtsalian@gmail.com>
2023-01-30 21:23:29 +0530

    Loader Revamp

--- client/src/Components/ProductLayout.jsx
+++ client/src/Components/ProductLayout.jsx
@@ -2,10 +2,8 @@ import React from "react";
 import {
   AsideProductContainer,
   AsideProductWrapper,
-  AsideWrapper,
   SectionProductContainer,
   SectionProductWrapper,
-  SectionWrapper,
   StyledProductMainWrapper,
 } from "../Styles/Product";
 import FilterDropdown from "./FilterDropdown";
