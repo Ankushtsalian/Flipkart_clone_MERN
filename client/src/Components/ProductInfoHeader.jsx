fa1a1f7db04df18ca6443d75d26f20d80a6f25e8
Ankush <ankushtsalian@gmail.com>
2023-01-31 21:23:54 +0530

    product header creation

--- client/src/Components/ProductInfoHeader.jsx
+++ client/src/Components/ProductInfoHeader.jsx
@@ -2,10 +2,26 @@ import React from "react";
 
 const ProductInfoHeader = () => {
   return (
-    <div style={{ borderBottom: "1px solid black" }}>
-      <div>PATH of product menu</div>
-      <div>Menu Header</div>
-      <div>Sort</div>
+    <div style={{ borderBottom: "1px solid black", padding: "10px" }}>
+      <div>
+        <div>PATH of product menu</div>
+      </div>
+      <div>
+        <div>Menu Header</div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "40%",
+        }}
+      >
+        <div style={{ padding: "10px 10px 10px 0" }}>Sort By</div>
+        <div style={{ padding: "10px 10px 10px 0" }}>Popularity</div>
+        <div style={{ padding: "10px 10px 10px 0" }}>Price -- Low to High</div>
+        <div style={{ padding: "10px 10px 10px 0" }}>Price -- High to Low</div>
+        <div style={{ padding: "10px 10px 10px 0" }}>Newest First</div>
+      </div>
     </div>
   );
 };
