efb87a4acc4031f9ff9f362aa9c12e16e996aaea
Ankush <ankushtsalian@gmail.com>
2023-02-02 22:11:36 +0530

    admin PAGE BASIC LAYOUT

--- client/src/Pages/AdminProduct.jsx
+++ client/src/Pages/AdminProduct.jsx
@@ -1,26 +1,88 @@
 import React from "react";
-
+import {
+  AsideProductContainer,
+  AsideProductWrapper,
+  ProductMainContainer,
+  SectionProductContainer,
+  SectionProductWrapper,
+  StyledProductMainWrapper,
+} from "../Styles/Product";
+import FilterDropdown from "../Components/FilterDropdown";
+import PriceRange from "../Components/PriceRange";
+import ProductInfo from "../Components/ProductInfo";
+import ProductInfoHeader from "../Components/ProductInfoHeader";
+import {
+  ProductMenuHeader,
+  ProductPath,
+  StyledProductInfoHeaderWrapper,
+} from "../Styles/ProductInfoHeader";
 const AdminProduct = () => {
+  const filterValue = [
+    "BRAND",
+    "COLOR",
+    "MATERIAL",
+    "AVAILABILITY",
+    "THEME",
+    "POPULAR COLLECTIONS",
+    "GST INVOICE AVAILABLE",
+  ];
   return (
-    <div
-      style={{
-        minHeight: "80vh",
-        maxWidth: "var(--width-max)",
-        margin: "0 auto",
-        paddingTop: "5px",
-      }}
-    >
+    <ProductMainContainer>
       <div
         style={{
-          minHeight: "80vh",
-          Width: "100%",
+          minWidth: "978px",
+          /* max-width: 1500px, */
+          width: "100%",
+          padding: " 0 10px 10px",
+          /* max-width: 1680px, */
+          fontsize: "13px",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "flex-start",
+
           margin: "0 auto",
-          backgroundColor: "var(--color-border-grey)",
-          wordWrap: "break-word",
+          gap: "10px",
+          minHeight: "100%",
+          marginBottom: "30px",
+          flex: "0 0 auto",
         }}
-      ></div>
-    </div>
+      >
+        <AsideProductWrapper>
+          <AsideProductContainer></AsideProductContainer>
+        </AsideProductWrapper>
+        <SectionProductWrapper>
+          <SectionProductContainer>
+            {/* <StyledProductInfoHeaderWrapper> */}
+            {/* <ProductPath>
+                <div>PATH of product menu</div>
+              </ProductPath> */}
+            {/* <ProductMenuHeader>
+                <div>Menu Header</div>
+              </ProductMenuHeader> */}
+            {/* </StyledProductInfoHeaderWrapper> */}
+          </SectionProductContainer>
+        </SectionProductWrapper>
+      </div>
+    </ProductMainContainer>
   );
 };
+// <div
+//   style={{
+//     minHeight: "80vh",
+//     maxWidth: "var(--width-max)",
+//     margin: "0 auto",
+//     paddingTop: "5px",
+//   }}
+// >
+//   <div
+//     style={{
+//       minHeight: "80vh",
+//       Width: "100%",
+//       margin: "0 auto",
+//       backgroundColor: "var(--color-border-grey)",
+//       wordWrap: "break-word",
+//     }}
+//   ></div>
+// </div>
 
 export default AdminProduct;
