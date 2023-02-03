4afb51ffd3a701554a61257ea25e0bd3d2a66c69
Ankush <ankushtsalian@gmail.com>
2023-02-03 12:41:15 +0530

    created admin panel for product

--- client/src/Components/AdminProductCreation.jsx
+++ client/src/Components/AdminProductCreation.jsx
@@ -1,3 +1,4 @@
+import { Input } from "@mui/material";
 import React from "react";
 import { filterValue } from "../Utils/productData";
 
@@ -6,26 +7,37 @@ const AdminProductCreation = () => {
     <div style={{ padding: "10px" }}>
       <div>
         <div>Admin header</div>
-        <form>
-          {filterValue.map((product, index) => (
-            <div
-              style={{
-                display: "flex",
-                width: "700px",
-                // border: "1px solid black",
-                justifyContent: "space-between",
-              }}
-              key={product}
-            >
-              <div style={{ margin: "10px", width: "30%", fontWeight: "bold" }}>
-                <span>{product} : </span>
+        <div style={{ margin: "10px", width: "100%", display: "grid" }}>
+          <form
+            style={{
+              margin: "0 auto",
+              padding: "20px",
+              //   width: "70%",
+              border: "1px solid black",
+            }}
+          >
+            {filterValue.map((product, index) => (
+              <div
+                style={{
+                  display: "flex",
+                  width: "700px",
+                  //   border: "1px solid black",
+                  justifyContent: "space-between",
+                }}
+                key={product}
+              >
+                <div
+                  style={{ margin: "10px", width: "30%", fontWeight: "bold" }}
+                >
+                  <span>{product} : </span>
+                </div>
+                <div style={{ margin: "10px", width: "70%" }}>
+                  <Input placeholder={product} style={{ width: "100%" }} />
+                </div>
               </div>
-              <div style={{ margin: "10px", width: "70%" }}>
-                <input />
-              </div>
-            </div>
-          ))}
-        </form>
+            ))}
+          </form>
+        </div>
       </div>
     </div>
   );
