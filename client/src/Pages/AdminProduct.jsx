b78cb7c0c1cf4fd1fb78da0dc238290a47d6547f
Ankush <ankushtsalian@gmail.com>
2023-02-10 21:08:51 +0530

    refractoring

--- client/src/Pages/AdminProduct.jsx
+++ client/src/Pages/AdminProduct.jsx
@@ -35,23 +35,5 @@ const AdminProduct = () => {
     </ProductMainContainer>
   );
 };
-// <div
-//   style={{
-//     minHeight: "80vh",
-//     maxWidth: "var(--width-max)",
-//     margin: "0 auto",
-//     paddingTop: "5px",
-//   }}
-// >
-//   <div
-//     style={{
-//       minHeight: "80vh",
-//       Width: "100%",
-//       margin: "0 auto",
-//       backgroundColor: "var(--color-border-grey)",
-//       wordWrap: "break-word",
-//     }}
-//   ></div>
-// </div>
 
 export default AdminProduct;
